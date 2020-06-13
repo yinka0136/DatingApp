dc95a35dd7dc0746e3fa24660581e9e9c5bdd770
Olayinka <yinka0136@gmail.com>
2020-06-13 13:03:03 +0100

    another commit

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -5,10 +5,10 @@ import { MessagesComponent } from "./messages/messages.component";
 import { UserlistComponent } from "./user/userlist/userlist.component";
 import { UserDashboardComponent } from "./user-dashboard/user-dashboard.component";
 import { AuthguardGuard } from "./_guards/authguard.guard";
-import { UserDetailsComponent } from './user/user-details/user-details.component';
-import { UserDetailResolver } from './_resolvers/user-detail.resolver';
-import { UserEditComponent } from './user/user-edit/user-edit.component';
-import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
+import { UserDetailsComponent } from "./user/user-details/user-details.component";
+import { UserDetailResolver } from "./_resolvers/user-detail.resolver";
+import { UserEditComponent } from "./user/user-edit/user-edit.component";
+import { PreventUnsavedChanges } from "./_guards/prevent-unsaved-changes.guard";
 export const appRoute: Routes = [
   { path: "home", component: HomeComponent },
   {
@@ -18,13 +18,21 @@ export const appRoute: Routes = [
     children: [
       { path: "list", component: ListComponent },
       { path: "messages", component: MessagesComponent },
-      { path: "userlist", component: UserlistComponent, pathMatch: 'full'},
-      { path: "user/:id", component: UserDetailsComponent ,resolve: {user: UserDetailResolver}},
-      { path: "users/edit/:id", component: UserEditComponent,resolve: {user: UserDetailResolver }, canDeactivate: [PreventUnsavedChanges]},
-
-    ]
+      { path: "userlist", component: UserlistComponent, pathMatch: "full" },
+      {
+        path: "user/:id",
+        component: UserDetailsComponent,
+        resolve: { i: UserDetailResolver },
+      },
+      {
+        path: "users/edit/:id",
+        component: UserEditComponent,
+        resolve: { user: UserDetailResolver },
+        canDeactivate: [PreventUnsavedChanges],
+      },
+    ],
   },
 
   { path: "dashboard", component: UserDashboardComponent },
-  { path: "**", redirectTo: "home", pathMatch: "full" }
+  { path: "**", redirectTo: "home", pathMatch: "full" },
 ];
